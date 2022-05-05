a6afd0b2c5c938c2b7124fc1cce781b3ae61b42a
TikhanovLeonid <masousmen@yandex.ru>
2022-05-05 08:42:02 +0300

    all_files

--- DJArbuz.py
+++ DJArbuz.py
@@ -1,8 +1,10 @@
 import discord
+from discord.utils import get
 from discord.ext import commands
 import logging
 import random
-import youtube_dl
+from youtube_dl import YoutubeDL
+import asyncio
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -48,8 +50,43 @@ class DJ_func(commands.Cog):
         num = random.randint(int(min_int), int(max_int))
         await ctx.send(num)
 
-    @commands.command(name="play")
+    @commands.command(name="p")
     async def play(self, ctx, query):
+        global voice
+        channel = ctx.message.author.voice.channel
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+        else:
+            await channel.connect(reconnect=True, timeout=300)
+        await ctx.send("YES")
+
+        with YoutubeDL({'outtmpl': '%(id)s.%(ext)s'}) as ydl:
+            result = ydl.extract_info(
+                query,
+                download=False  # We just want to extract the info
+            )
+
+        if 'entries' in result:
+            # Can be a playlist or a list of videos
+            info = result['entries'][0]
+        else:
+            # Just a video
+            info = result
+
+        print(info)
+        video_url = info['url']
+        print(video_url)
+        await ctx.send(URL)
+        voice.play(discord.FFmpegPCMAudio(source=URL, executable="ffmpeg.exe"),
+                   **{'format': 'worstaudio/best',
+                      'noplaylist': 'True', 'simulate': 'True', 'preferredquality': '192', 'preferredcodec': 'mp3',
+                      'key': 'FFmpegExtractAudio'})
+
+        while voice.is_playing():
+            await asyncio.sleep(1)
+        if not voice.is_paused():
+            await voice.disconnect()
 
 
 bot = commands.Bot(command_prefix='!', intents=intents)
