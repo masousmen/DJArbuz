491cf59f066b656a8b02129574895f322bc10e02
masousmen <87076759+masousmen@users.noreply.github.com>
2022-05-09 17:36:36 +0300

    Update DJArbuz.py

--- DJArbuz.py
+++ DJArbuz.py
@@ -7,19 +7,6 @@ from youtube_dl import YoutubeDL
 from collections import deque
 import asyncio
 
-<<<<<<< HEAD
-=======
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
-intents = discord.Intents.default()
-intents.members = True
-
-TOKEN = "TOKEN"
-
->>>>>>> origin/master
 
 class YLBotClient(discord.Client):
     async def on_ready(self):
